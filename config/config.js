7b0cf4da5eae2ef2320e0bfa6915fe6e77a2a411
Toan <nktoanwork@gmail.com>
2024-01-06 21:32:53 +0700

    Done to deploy

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
-    username: "root",
-    password: "Toannguyen632@",
-    database: "final_cloud",
-    host: "localhost",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
   production: {
